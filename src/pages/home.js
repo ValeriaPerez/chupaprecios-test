867f43d0531b08680b45fefbc4e5d8fd1873014c
Valeria Pérez <valeria.pjaimes@gmail.com>
2023-10-14 12:18:22 -0600

    Add alert component

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,15 @@
-import { useState } from 'react';
-import { Stack, Container, Grid, ImageList, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Alert,
+  Container,
+  Grid,
+  ImageList,
+  Snackbar,
+  Stack,
+  Typography,
+  IconButton,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { useGetProducts } from '../api/products';
 import { useCart } from '../hooks';
@@ -16,6 +26,7 @@ const Home = () => {
   const { setIsCart } = useCart();
   const [detailModal, setDetailModal] = useState();
   const [open, setOpen] = useState(false);
+  const [openSnackBar, setOpenSnackBar] = useState(false);
   const [store, setStore] = useState('amazon');
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState('gato');
@@ -28,8 +39,12 @@ const Home = () => {
   const handleClose = () => {
     setOpen(false);
   };
+  const handleCloseSnackbar = () => {
+    setOpenSnackBar(!openSnackBar);
+  };
   const onClickCart = (data) => {
     setIsCart(data);
+    handleCloseSnackbar();
   };
   const handleChangePage = (action) => {
     if (action === 'next') {
@@ -39,6 +54,12 @@ const Home = () => {
       setPage(page - 1);
     }
   };
+
+  const action = (
+    <React.Fragment>
+      <CloseIcon fontSize='small' onClick={() => setOpenSnackBar(false)}/>
+    </React.Fragment>
+  );
 
   return (
     <div className='App'>
@@ -85,6 +106,28 @@ const Home = () => {
           setPage={setPage}
         />
       </Container>
+      <Snackbar
+        open={openSnackBar}
+        autoHideDuration={2000}
+        onClose={handleCloseSnackbar}
+        action={action}
+      >
+        <Alert
+          variant='filled'
+          severity='success'
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => setOpenSnackBar(false)}
+            >
+              <CloseIcon fontSize='small' />
+            </IconButton>
+          }>
+          Se agrego tu producto al carrito
+        </Alert>
+      </Snackbar>
       <Modal
         data={detailModal}
         handleClose={handleClose}
